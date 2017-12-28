f0dcbbad13c7a976d12e41dc6ef731c050cc720a
nao-pon <hypweb@gmail.com>
2017-12-28 11:58:18 +0900

    [preload] fox notice error of debugOnlyAdmin

--- html/preload/debugOnlyAdmin.class.php
+++ html/preload/debugOnlyAdmin.class.php
@@ -6,7 +6,7 @@
 class debugOnlyAdmin extends XCube_ActionFilter
 {
     public function postFilter() {
-        if ($GLOBALS['xoopsErrorHandler'] && is_object($GLOBALS['xoopsErrorHandler'])) {
+        if (!empty($GLOBALS['xoopsErrorHandler']) && is_object($GLOBALS['xoopsErrorHandler'])) {
             $root = XCube_Root::getSingleton();
             if (! $root->mContext->mUser->isInRole('Site.Administrator')) {
                 $GLOBALS['xoopsErrorHandler']->activate(false);
